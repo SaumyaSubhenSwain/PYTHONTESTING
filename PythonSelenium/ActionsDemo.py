9d3cf39ad9eb4f402626aec3eb7d0ed85e7622b2
QA-saumyasubhen <swainsaumyasubhen@gmail.com>
2025-12-18 23:26:00 +0530

    few changes

--- PythonSelenium/ActionsDemo.py
+++ PythonSelenium/ActionsDemo.py
@@ -22,3 +22,6 @@ time.sleep(2)
 action.move_to_element(driver.find_element(By.ID,"mousehover")).perform()
 # action.move_to_element(driver.find_element(By.LINK_TEXT, "Top")).perform()
 action.move_to_element(driver.find_element(By.LINK_TEXT, "Reload")).click().perform()
+
+
+print("fhfadajdkadkjwd")
